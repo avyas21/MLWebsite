af4c47be72546a831b5bc04d40081fb6d6dfc211
user <akhilvimalvyas@gmail.com>
2020-07-26 17:06:32 -0500

    finished add image and filter visualization

--- src/app/cnn/cnn.component.ts
+++ src/app/cnn/cnn.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef} from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
 import { NgForm } from '@angular/forms';
 
@@ -11,6 +11,12 @@ export class CNNComponent implements AfterViewInit {
   private context: CanvasRenderingContext2D;
   @ViewChild('canvas') canvas: ElementRef;
 
+  private context_filter: CanvasRenderingContext2D;
+  @ViewChild('canvas_filter') canvas_filter: ElementRef;
+
+  private context_feature: CanvasRenderingContext2D;
+  @ViewChild('canvas_feature') canvas_feature: ElementRef;
+
   isLayerSelected = false;
   selectedLayer = 0;
   inputChannel = 1;
@@ -19,10 +25,26 @@ export class CNNComponent implements AfterViewInit {
   maxOut = 0;
   showConv = false;
   showPooling = false;
+  shownOriginal = false;
+  inputImageData: ImageData;
 
+  constructor(private cdRef : ChangeDetectorRef) {
+    this.cdRef = cdRef;
+  }
+
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
+  }
 
   ngAfterViewInit(): void {
-    this.context = (this.canvas.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.context = (this.canvas.nativeElement as
+      HTMLCanvasElement).getContext('2d');
+    this.context_filter = (this.canvas_filter.nativeElement as
+       HTMLCanvasElement).getContext('2d');
+
+    this.context_feature = (this.canvas_feature.nativeElement as
+      HTMLCanvasElement).getContext('2d');
+
     this.visualizeLayers();
   }
 
@@ -83,6 +105,8 @@ export class CNNComponent implements AfterViewInit {
       this.context.fillText(shapes, 125, y+35);
       y += 50;
     }
+
+    this.initialVisual();
   }
 
 
@@ -92,30 +116,71 @@ export class CNNComponent implements AfterViewInit {
     this.maxInp = dimensions[2];
     this.maxOut = dimensions[3];
     this.showConv = true;
+
+    this.canvas_filter.nativeElement.width = dimensions[1]*50 + 100;
+    this.canvas_filter.nativeElement.height = dimensions[0]*50 + 100;
+    var imgData = this.context_filter.createImageData(dimensions[0], dimensions[1]);
     var string = '';
     string += '-----------------------------------------\n';
+
+    var max = arr[0][0][inp][out];
+    var min = arr[0][0][inp][out];
     for(var i = 0; i < dimensions[0]; ++i) {
       for(var j = 0; j < dimensions[1]; ++j) {
         string += '| ' + arr[i][j][inp][out].toFixed(2).toString().padStart(5,'+') + ' ';
+        if(arr[i][j][inp][out] > max) {
+          max = arr[i][j][inp][out];
+        }
+        if(arr[i][j][inp][out] < min) {
+          min = arr[i][j][inp][out];
+        }
       }
         string += '|\n';
     }
     string += '-----------------------------------------\n';
+
+    for(var i = 0; i < dimensions[0]; ++i) {
+      for(var j = 0; j < dimensions[1]; ++j) {
+        imgData.data[4 * (i * dimensions[1] + j) + 0] = (arr[i][j][inp][out]-min)/(max-min)*255;
+        imgData.data[4 * (i * dimensions[1] + j) + 1] = (arr[i][j][inp][out]-min)/(max-min)*255;
+        imgData.data[4 * (i * dimensions[1] + j) + 2] = (arr[i][j][inp][out]-min)/(max-min)*255;
+        imgData.data[4 * (i * dimensions[1] + j) + 3] = 255;
+      }
+    }
+
+
+    this.scaleImageData(imgData, 50, this.context_filter);
+
+    this.visualizeFeature("assets/img_1.jpg");
     return string;
   }
 
   showPooling2d(layer_num) {
-    return "THIS IS A POOLONG :AEIT";
+    var pool = this.model.layers[layer_num].getConfig().poolSize;
+    var stride = this.model.layers[layer_num].getConfig().strides;
+
+    var string = '';
+    var line =  '-----'.repeat(pool[1]);
+
+    for(var i = 0; i < pool[0]; ++i) {
+      string += line + '\n';
+      for(var j = 0; j < pool[1]; ++j) {
+        string += '| 0 ';
+      }
+      string += '|\n';
+
+    }
+
+    string += line;
+    return string;
   }
 
   showLayerInfo(layer_num) {
-    console.log("!: " + this.model.layers[layer_num].name);
     this.showConv = false;
     this.showPooling = false;
 
 
     if(this.model.layers[layer_num].name.startsWith('conv2d')) {
-      console.log("2: " + this.model.layers[layer_num].name);
       this.showConv2d(layer_num, 0, 0);
       return;
     }
@@ -163,5 +228,64 @@ export class CNNComponent implements AfterViewInit {
 
     this.model.layers[this.selectedLayer].setWeights([tf.tensor(arr),tf.tensor(bias)]);
 
+  }
+
+  scaleImageData(imageData, scale, context) {
+    var scaled = context.createImageData(imageData.width * scale, imageData.height * scale);
+
+    for(var row = 0; row < imageData.height; row++) {
+      for(var col = 0; col < imageData.width; col++) {
+        var sourcePixel = [
+          imageData.data[(row * imageData.width + col) * 4 + 0],
+          imageData.data[(row * imageData.width + col) * 4 + 1],
+          imageData.data[(row * imageData.width + col) * 4 + 2],
+          imageData.data[(row * imageData.width + col) * 4 + 3]
+        ];
+        for(var y = 0; y < scale; y++) {
+          var destRow = row * scale + y;
+          for(var x = 0; x < scale; x++) {
+            var destCol = col * scale + x;
+            for(var i = 0; i < 4; i++) {
+              scaled.data[(destRow * scaled.width + destCol) * 4 + i] =
+                sourcePixel[i];
+            }
+          }
+        }
+      }
+    }
+
+    context.putImageData(scaled, 0, 0);
+    return scaled;
+  }
+
+  initialVisual() {
+    if(this.model.layers[0].name.startsWith('conv2d')) {
+      this.showConv2d(0, 0, 0);
+      return;
+    }
+
+    if(this.model.layers[0].name.startsWith('max_pooling2d')) {
+      this.showPooling = true;
+      return;
+    }
+  }
+
+  visualizeFeature(path) {
+    if(!this.shownOriginal) {
+      this.shownOriginal = true;
+      const image = new Image;
+      image.src = path;
+      image.onload = () => {
+        this.context_feature.drawImage(image, 0, 0);
+        this.inputImageData = this.context_feature.getImageData(0, 0, image.height,
+        image.width);
+
+        this.canvas_feature.nativeElement.width = image.width*10 + 100;
+        this.canvas_feature.nativeElement.height = image.height*20 + 200;
+
+        this.context_feature.scale(8,8);
+        this.context_feature.drawImage(image, 0, 0);
+      }
+    }
   }
 }
